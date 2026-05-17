374cc4709593464885f47c6fc9368dbfab9f620a
ankit200819 <ankitkumar2242001@gmail.com>
2026-05-17 22:29:58 +0530

    Fix: dynamic API URL + add Render deployment config

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,6 @@
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+  ? 'http://localhost:5000/api'
+  : '/api';
 
 const api = {
   async request(path, { method = 'GET', body, auth = false } = {}) {
