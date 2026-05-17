9254eef527d12691dd533e2b0809f6ff1b81da2d
ankit200819 <ankitkumar2242001@gmail.com>
2026-05-17 21:51:14 +0530

    Initial commit

--- updateImg.js
+++
@@ -1,5 +0,0 @@
-const { DatabaseSync } = require('node:sqlite');
-const path = require('path');
-const db = new DatabaseSync(path.join(__dirname, 'backend', 'database.db'));
-db.prepare("UPDATE products SET images = '[\"/assets/nikedunklow.png\"]' WHERE name LIKE '%Nike Dunk Low%'").run();
-console.log("Updated Nike Dunk Low image");
